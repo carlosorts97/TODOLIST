b656e903e86f980fa1db4d97e5ac38012fa2915d
carlosorts97 <32301526+carlosorts97@users.noreply.github.com>
2017-11-15 10:46:54 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+# TODOLIST
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title></title>
+    <style>
+
+
+    body{font-family: monospace;}
+
+    div{background-color: Gainsboro;
+        width: 350px;
+        height: 100px;
+        border-radius: 5px;}
+
+    div a{text-align: center;
+          margin-left: 30px;
+          display: inline-flex;
+          margin-top: 30px;
+          text-decoration: none;
+          font-size: 13px;
+          background-color:white;
+          border-radius: 5px;
+          padding: 10px;
+
+
+    }
+    </style>
+  </head>
+  <body>
+    <h1>Bienvenido a TODOLIST</h1>
+    <div>
+
+      <a href="signup.php">Registrate!</a>
+      <a href="signin.php">Inicia Sesion!</a>
+    </div>
+
+  </body>
+</html>
