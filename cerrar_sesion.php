5a4e02a9bed4ecaffeb970c276020f9eefc8909c
carlosorts97 <32301526+carlosorts97@users.noreply.github.com>
2017-11-15 10:38:44 +0100

    Create cerrar_sesion.php

---
+++ cerrar_sesion.php
@@ -0,0 +1,7 @@
+# TODOLIST/acciones
+# Este archivo se encuentra en la carpeta acciones.
+<?php
+//Limpiamos las variables de Sesion y redirigimos al index.
+session_destroy();
+header('Location:../index.php');
+ ?>
