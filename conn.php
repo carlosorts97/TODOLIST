2f12e51182fc34c926a61be5367bede80a34786f
carlosorts97 <32301526+carlosorts97@users.noreply.github.com>
2017-11-22 12:59:25 +0100

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,17 +1,14 @@
 # TODOLIST
 
 <?php
-
-$ip_domain=$_SERVER['SERVER_ADDR'];
-if($ip_domain=='127.0.0.0'){
-  $conf='config/config.ini';
+$ip_domain=$_SERVER['SERVER_NAME'];
+if($ip_domain=='corts.cesnuria.com'){
+  $conf='config/config.ini.dev';
 }
 else{
   $conf='config/config.ini';
 }
-
 $config=parse_ini_file($conf);
-
 try{
   $conn = new mysqli($config['dbhost'],$config['dbuser'],$config['dbpass'],$config['dbname']);
 }catch(Excepcion$e){
